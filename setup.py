83ada59e43de020b478f97df2e188cea14488c76
Orne Brocaar <info@brocaar.com>
2012-11-07 09:39:09 +0100

    Make Job-Runner a Python package.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+import job_runner
+
+
+setup(
+    name='job-runner',
+    version=job_runner.__version__,
+    url='http://github.com/spilgames/jobrunner',
+    author='Orne Brocaar',
+    author_email='orne.brocaar@spilgames.com',
+    description='Tool for scheduling jobs and realtime monitoring',
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=['manage.py'],
+    install_requires=[
+        'django==1.4.2',
+        'django-grappelli',
+        'south',
+        'django-tastypie==0.9.12-alpha',
+        'mimeparse==0.1.3',
+        'python-dateutil==1.5',
+        'requests==0.13.1',
+    ],
+    dependency_links=[
+        'https://github.com/toastdriven/django-tastypie/tarball/27d17b7db7afd6c81da24f64f5b4562b59134582#egg=django-tastypie-0.9.12-alpha',
+    ]
+)
