1ff5bca57d2ac845607f04859233d95815471bd9
Orne Brocaar <info@brocaar.com>
2012-11-19 10:37:52 +0100

    Add WebKit browser notifications.

--- job_runner/apps/job_runner/static/job_runner/js/views/runs.js
+++ job_runner/apps/job_runner/static/job_runner/js/views/runs.js
@@ -11,6 +11,7 @@ var RunView = Backbone.View.extend({
     initialize: function(options) {
         _.bindAll(this, 'renderRun', 'changeRun', 'initialFetch', 'initialFetchRuns', 'sortRuns', 'handleEvent');
         this.activeProject = null;
+        this.notifier = options.notifier;
 
         this.workerCollection = new WorkerCollection();
         this.jobTemplateCollection = new JobTemplateCollection();
@@ -27,6 +28,7 @@ var RunView = Backbone.View.extend({
             $('#job_runner section').addClass('hide');
             $('#dashboard').removeClass('hide');
             self.activeProject = options.projectCollection.get(project_id);
+            self.notifier.RequestPermission();
 
             self.workerCollection.reset();
             self.jobTemplateCollection.reset();
@@ -195,6 +197,10 @@ var RunView = Backbone.View.extend({
 
     // callback used when an item has been changed
     changeRun: function(run) {
+        var job = this.jobCollection.where({resource_uri: run.attributes.job})[0];
+
+        this.notifier.Notify("", "Job-Runner", job.attributes.title + " changed to " + run.humanReadableState());
+
         var self = this;
         $('#run-' + run.id, self.el).fadeOut('fast', function() {
             $(this).remove();
