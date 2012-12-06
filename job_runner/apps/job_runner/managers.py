042138eec7128b1026a1ef0a78847facc0f60587
Orne Brocaar <info@brocaar.com>
2012-12-06 10:52:07 +0100

    Move logic behind selecting enqueueable runs to RunManager class.

--- job_runner/apps/job_runner/managers.py
+++ job_runner/apps/job_runner/managers.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import Q
+from django.utils import timezone
 
 
 class RunManager(models.Manager):
@@ -7,7 +9,30 @@ class RunManager(models.Manager):
     """
     def scheduled(self):
         """
-        Return a QS filtered on run's that aren't enqueued yet.
+        Return a QS filtered on run's are in the scheduled state.
         """
         qs = self.get_query_set()
         return qs.filter(enqueue_dts__isnull=True)
+
+    def enqueueable(self):
+        """
+        Return a QS filterd on runs's that are ready to enqueue.
+        """
+        job_qs = self.model.job.get_query_set()
+
+        active_jobs = job_qs.filter(
+            run__enqueue_dts__isnull=False,
+            run__return_dts__isnull=True,
+        )
+        scheduled = self.scheduled()
+
+        return scheduled.filter(
+            # make sure it should be running now
+            schedule_dts__lte=timezone.now(),
+        ).exclude(
+            # exclude auto scheduled jobs when enqueue is disabled
+            Q(job__enqueue_is_enabled=False, is_manual=False) |
+
+            # exclude jobs that are still active
+            Q(job__in=active_jobs)
+        )
