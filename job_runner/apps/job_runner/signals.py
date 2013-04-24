b0a850376a8da85e8aebf7f8c3c1e1440c996bfc
Orne Brocaar <info@brocaar.com>
2013-04-24 11:37:47 +0200

    Lock sibling rows to make sure that this part can not run in parallel.
    When two transactions are overlapping, it is possible that it is never
    detected that all siblings finished, and thus the job will not be
    re-scheduled anymore.

--- job_runner/apps/job_runner/signals.py
+++ job_runner/apps/job_runner/signals.py
@@ -43,8 +43,10 @@ def post_run_update(sender, instance, created, raw, **kwargs):
 
             job.save()
 
-        unfinished_siblings = instance.get_siblings().filter(
-            return_dts__isnull=True).count()
+        # on purpose we are not using .count() since with that, the
+        # .select_for_update() does not have any effect.
+        unfinished_siblings = len(instance.get_siblings().filter(
+            return_dts__isnull=True).select_for_update())
 
         if not unfinished_siblings:
             job.reschedule()
