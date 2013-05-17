029614009d290867de99478f1b75de1182d88eaa
Orne Brocaar <info@brocaar.com>
2013-05-17 15:35:20 +0200

    Add test to test mark_failed on completed run.

--- job_runner/apps/job_runner/tests/functional/test_models.py
+++ job_runner/apps/job_runner/tests/functional/test_models.py
@@ -291,3 +291,20 @@ class RunTestCase(TestCase):
         self.assertIsInstance(run.start_dts, datetime)
         self.assertIsInstance(run.return_dts, datetime)
         self.assertFalse(run.return_success)
+
+    def test_mark_failed_on_completed_run(self):
+        """
+        Test :meth:`.Run.mark_failed` on completed run.
+
+        In this case, :meth:`.Run.mark_failed` shouldn't do anything.
+
+        """
+        Run.objects.filter(pk=1).update(
+            return_dts=timezone.now(),
+            return_success=True,
+        )
+        run = Run.objects.get(pk=1)
+        run.mark_failed('Test mark failed')
+        run = Run.objects.get(pk=1)
+
+        self.assertTrue(run.return_success)
