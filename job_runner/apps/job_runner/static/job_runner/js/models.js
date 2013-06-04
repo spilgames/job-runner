2d9f668155bde0a079bd098ff3fd45e9f1b4f8ff
Orne Brocaar <info@brocaar.com>
2013-06-04 13:51:01 +0200

    Remove recursion check again (since this is now done on model level).

--- job_runner/apps/job_runner/static/job_runner/js/models.js
+++ job_runner/apps/job_runner/static/job_runner/js/models.js
@@ -263,22 +263,15 @@ angular.module('job', ['ngResource', 'getAll', 'jobTemplate', 'workerPool', 'mod
     };
 
     // Return the absolute parent of this job-chain
-    Job.prototype.get_absolute_parent = function(success, seen) {
-        if (!seen) {
-            seen = [];
-        }
+    Job.prototype.get_absolute_parent = function(success) {
         var absolute_parent = null;
         if (this.parent) {
             var parentId = this.parent.split('/').splice(-2, 1)[0];
             Job.get({id: parentId}, function(job) {
-                if (seen.indexOf(job) == -1) {
-                    seen.push(job);
-
-                    if(job.parent) {
-                        absolute_parent = job.get_absolute_parent(success, seen);
-                    } else {
-                        absolute_parent = job;
-                    }
+                if(job.parent) {
+                    absolute_parent = job.get_absolute_parent(success);
+                } else {
+                    absolute_parent = job;
                 }
             });
         } else {
